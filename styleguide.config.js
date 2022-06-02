0ca6214a77807c5050b1da3028a8af6b6e79a058
anovandri <aditya.novandri@dkatalis.com>
2022-06-02 12:59:09 +0700

    fix: test

--- styleguide.config.js
+++ styleguide.config.js
@@ -8,11 +8,11 @@ if (fs.statSync(componentsDir).isDirectory()) {
   const componentItems = fs.readdirSync(componentsDir);
   componentItems.forEach((componentName) => {
     const fullItemPath = path.join(componentsDir, componentName);
-    if (fs.statSync(fullItemPath).isDirectory()) {
-      const versionItems = fs.readdirSync(fullItemPath);
-      versionItems.forEach((versionName) => {
-        componentTree[componentName] = componentTree[componentName] || {};
-        componentTree[componentName][versionName] = path.join("./package/src/components", componentName, versionName, `${componentName}.js`);
+        if (fs.statSync(fullItemPath).isDirectory()) {
+            const versionItems = fs.readdirSync(fullItemPath);
+              versionItems.forEach((versionName) => {
+                componentTree[componentName] = componentTree[componentName] || {};
+                componentTree[componentName][versionName] = path.join("./package/src/components", componentName, versionName, `${componentName}.js`);
       });
     }
   });
