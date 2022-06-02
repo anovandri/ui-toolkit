a06b63544ade5b3afdc93c8e27a936d5dfec29db
anovandri <aditya.novandri@dkatalis.com>
2022-06-02 13:00:10 +0700

    fix: add new variable

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,16 +3,17 @@ const fs = require("fs");
 
 const componentsDir = path.join(__dirname, "package/src/components");
 const componentTree = {};
+const a = {};
 
 if (fs.statSync(componentsDir).isDirectory()) {
   const componentItems = fs.readdirSync(componentsDir);
   componentItems.forEach((componentName) => {
     const fullItemPath = path.join(componentsDir, componentName);
-        if (fs.statSync(fullItemPath).isDirectory()) {
-            const versionItems = fs.readdirSync(fullItemPath);
-              versionItems.forEach((versionName) => {
-                componentTree[componentName] = componentTree[componentName] || {};
-                componentTree[componentName][versionName] = path.join("./package/src/components", componentName, versionName, `${componentName}.js`);
+    if (fs.statSync(fullItemPath).isDirectory()) {
+      const versionItems = fs.readdirSync(fullItemPath);
+      versionItems.forEach((versionName) => {
+        componentTree[componentName] = componentTree[componentName] || {};
+        componentTree[componentName][versionName] = path.join("./package/src/components", componentName, versionName, `${componentName}.js`);
       });
     }
   });
